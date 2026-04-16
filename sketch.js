ddb6fccf32d2c8e960339bf8d1bc6cb2506fea38
Mareruma <tolpeznikovs.marcis@gmail.com>
2026-04-16 20:28:01 +0300

    Pievienota mērvienību parādīšana, sakārtots/izdzēsts kods.

--- sketch.js
+++ sketch.js
@@ -1,152 +1,105 @@
-    let kermenis, attels;
-    let d, f, F;
-    let x, y;
-    let H, h;
+// https://www.w3schools.com/CSS//css_padding.asp
+// https://phet.colorado.edu/sims/html/geometric-optics/latest/geometric-optics_all.html
+// https://p5js.org/reference/p5/text/
+// https://p5js.org/reference/p5/round/
+// CVG fizika, Tēma: "Optika"
 
-    function setup() {
-        new Canvas(400, 400);
+let d, f, F;
+let x, y;
+let H, h;
 
-        // kermenis = new Sprite();
-        // kermenis.strokeWeight = 0;
-        // kermenis.x = 30;
-        // kermenis.y = 30;
-        // kermenis.w = 50;
-        // kermenis.h = 50;
-        // kermenis.color = "red";
+function setup() {
+    new Canvas(400, 500);
 
-        // attels = new Sprite();
-        // attels.strokeWeight = 0;
-        // attels.x = 370;
-        // attels.y = 370;
-        // attels.w = 50;
-        // attels.h = 50;
-        // attels.color = "green";
+    F = 50;
+}
 
-        F = 50;
+function update() {
+    background("white");
 
+    x = mouseX;
+    y = mouseY;
+
+    // Hitbox pārbaudes
+    if (x >= 197) {
+        x = 196;
+    } else if (x <= 10) {
+        x = 11;
+    }
+    if (y >= 390) {
+        y = 389;
+    } else if (y <= 10) {
+        y = 11;
     }
 
-    function update() {
+    // Asis
+    strokeWeight(1);
+    stroke("black");
+    line(0, 200, 400, 200);
+    line(200, 0, 200, 400);
+    
+    //lēca
+    strokeWeight(7);
+    stroke("lightblue");
+    line(200, 150, 200, 250);
 
-        background("white");
+    // Fokusa attālums 1
+    strokeWeight(5);
+    stroke("black");
+    point(200 - F, 200);
 
-        // kermenis.x = mouseX;
-        // kermenis.y = mouseY;
+    // Fokusa attālums 2
+    strokeWeight(5);
+    stroke("black");
+    point(200 + F, 200);
 
-        x = mouseX;
-        y = mouseY;
+    // 1. Principiālais stars
+    strokeWeight(1);
+    stroke("red");
+    line(x, y, 200, y);
+    line(200, y, 200 + f, 200 + H);
 
-        if (x >= 197) {
-            x = 196;
-        }
-        else if (x <= 10) {
-            x = 11;
-        }
+    // 2. Principiālais stars
+    strokeWeight(1);
+    stroke("blue");
+    line(x, y, 200 + f, 200 + H);
 
-        if (y >= 390) {
-            y = 389;
-        }
-        else if (y <= 10) {
-            y = 11;
-        }
+    // 3. Principiālais stars
+    strokeWeight(1);
+    stroke("green");
+    line(x, y, 200, 200 + H);
+    line(200, 200 + H, 200 + f, 200 + H);
 
-        // Rāmis
-        strokeWeight(1);
-        stroke("black");
-        line(0, 200, 400, 200);
-        line(200, 0, 200, 400);
-        
+    // Nepieciešamo lielumu aprēķini
+    d = 200 - x;
+    f = (F * d) / (d - F);
+    h = 200 - y;
+    H = (f * h) / d;
 
-        //lēca
-        strokeWeight(7);
-        stroke("lightblue");
-        line(200, 150, 200, 250);
+    // Ķermeņa līnija
+    strokeWeight(3);
+    stroke("black");
+    line(x, y, x, 200);
 
-        // Fokusa attālums 1
-        strokeWeight(5);
-        stroke("black");
-        point(200 - F, 200);
-
-        // Fokusa attālums 2
-        strokeWeight(5);
-        stroke("black");
-        point(200 + F, 200);
-
-        // // 1. Principiālais stars
-        // strokeWeight(1);
-        // stroke("red");
-        // line(kermenis.x, kermenis.y, 200, kermenis.y);
-        // line(200, kermenis.y, attels.x, attels.y);
-
-        // // 2. Principiālais stars
-        // strokeWeight(1);
-        // stroke("blue");
-        // line(kermenis.x, kermenis.y, attels.x, attels.y);
-
-        // // 3. Principiālais stars
-        // strokeWeight(1);
-        // stroke("green");
-        // line(kermenis.x, kermenis.y, 200, attels.y);
-        // line(200, attels.y, attels.x, attels.y);
-
-        // 1. Principiālais stars
-        strokeWeight(1);
-        stroke("red");
-        line(x, y, 200, y);
-        line(200, y, 200 + f, 200 + H);
-
-        // 2. Principiālais stars
-        strokeWeight(1);
-        stroke("blue");
-        line(x, y, 200 + f, 200 + H);
-
-        // 3. Principiālais stars
-        strokeWeight(1);
-        stroke("green");
-        line(x, y, 200, 200 + H);
-        line(200, 200 + H, 200 + f, 200 + H);
-
-        d = 200 - x;
-        f = (F * d) / (d - F);
-
-        h = 200 - y;
-
-        H = (f * h) / d;
-
-        // Ķermeņa līnija
-        strokeWeight(3);
-        stroke("black");
-        line(x, y, x, 200);
-
-        // Attēla līnija
-        strokeWeight(3);
-        stroke("gray");
-        line(200 + f, 200, 200 + f, 200 + H);
+    // Attēla līnija
+    strokeWeight(3);
+    stroke("gray");
+    line(200 + f, 200, 200 + f, 200 + H);
 
 
+    // Mērvienību rādīšana
+    strokeWeight(1);
+    stroke("black");
+    rect(0, 400, 405, 105);
+    strokeWeight(0);
+    textSize(15);
+    text("F = "+ F + "px", 10, 415);
+    text("d = " + d + "px", 10, 430);
+    text("f = " + round(f) + "px", 10, 445);
+    text("H = " + round(H) + "px", 10, 460);
+    text("h = " + h + "px", 10, 475);
 
+    text("x = " + x, 210, 415);
+    text("y = " + y, 210, 430);
 
-        // d = 200 - kermenis.x;
-        // if(d < 0){
-        //     d *= -1;
-        // }
-        // f = 400 - attels.x;
-        // if(f < 0){
-        //     f *= -1;
-        // }
-        
-
-        
-        // attels.x = 400 - kermenis.x;
-        // attels.y = 400 - kermenis.y;
-
-        // if (kb.presses(" ")) {
-        //     paraditAprekinus(); 
-        // }
-    }
-
-    // function paraditAprekinus() {
-    //     alert("F = " + F + "px" + "\n" + "f = " + f + "px" + "\n" + "d = " + d + "px");
-        
-
-    // }
+}
